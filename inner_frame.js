0edcfca872b615d6be60ba338a65e2c327e318d7
Rotsteinfuchs <169158948+FoxedDev@users.noreply.github.com>
2025-06-14 20:16:08 +0200

    Move loading icon to style sheet

--- inner_frame.js
+++ inner_frame.js
@@ -1,7 +1,3 @@
 document
   .querySelectorAll('style, link[rel="stylesheet"]')
   .forEach((e) => e.remove());
-
-const loading_img = document.createElement("img");
-loading_img.src = browser.runtime.getURL("images/loading_animation.gif");
-document.body.appendChild(loading_img);
